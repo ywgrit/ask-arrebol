f0c7e0dc2728203ccfa634fb12ce342cc87a0c21
Xin Wang <yw987194828@gmail.com>
2025-05-19 14:51:36 +0800

    replace image when yes is press

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ yesButton.addEventListener("click", function () {
   document.body.innerHTML = `
         <div class="yes-screen">
             <h1 class="yes-text"></h1>
-            <img src="images/hug.png" alt="拥抱" class="yes-image">
+            <img src="images/aixin-nobg.png" alt="爱心" class="yes-image">
         </div>
     `;
 
